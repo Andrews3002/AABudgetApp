68aaceb0670f18df63c4a605a5de89717b875952
Andrews3002 <alexangeloandrews@gmail.com>
2026-05-24 07:20:34 -0400

    feature: electron ui setup complete

--- electron/main.ts
+++ electron/main.ts
@@ -1,1 +1,18 @@
-console.log("Hello World from Electron");
+import { app, BrowserWindow } from "electron";
+
+const createWindow = () => {
+    const window = new BrowserWindow({
+        width: 1200,
+        height: 780
+    });
+
+    //PROD
+    // window.loadFile("index.html");
+
+    //DEV
+    window.loadURL("http://localhost:5173/");
+}
+
+app.whenReady().then(()=>{
+    createWindow();
+});
