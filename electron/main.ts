c8f10f9211940e89388d29607dec9a27b7849c08
Andrews3002 <alexangeloandrews@gmail.com>
2026-05-25 15:08:22 -0400

    feature: preload script setup successfully

--- electron/main.ts
+++ electron/main.ts
@@ -1,9 +1,19 @@
 import { app, BrowserWindow } from "electron";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export { __filename, __dirname };
 
 const createWindow = () => {
     const window = new BrowserWindow({
         width: 1200,
-        height: 780
+        height: 780,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js")
+        },
     });
 
     //PROD
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const information = document.getElementById("info");
+information.innerText = `This app is using Chrome (v${window.versions.chrome()}), Node.js (v${window.versions.node()}), and Electron (v${window.versions.electron()})`;
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
